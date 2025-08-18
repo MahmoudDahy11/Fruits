6a3716c44bdd1366eeee7cdd781dcd2ef98998a6
Mahmoud Dahy <dahym2028@gmail.com>
2025-08-18 14:29:42 +0300

    create custom list tile info

--- lib/features/shopping/presentation/views/home_view_product.dart
+++ lib/features/shopping/presentation/views/home_view_product.dart
@@ -1,5 +1,4 @@
-import 'package:e_commerce_app/core/constant/assets.dart';
-import 'package:e_commerce_app/core/constant/constant.dart';
+import 'package:e_commerce_app/features/shopping/presentation/views/widgets/custom_list_tile_info.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewProduct extends StatelessWidget {
@@ -8,48 +7,7 @@ class HomeViewProduct extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            ListTile(
-              leading: Image.asset(Assets.imagesAvatar),
-              title: Text(
-                'صباح الخير!..',
-                style: TextStyle(
-                  fontFamily: 'Cairo',
-                  fontSize: 20,
-                  color: Colors.grey,
-                  fontWeight: FontWeight.normal,
-                ),
-              ),
-              subtitle: Text(
-                'محمود ضاحي',
-                style: TextStyle(
-                  fontFamily: 'Cairo',
-                  fontSize: 24,
-                  color: Colors.black,
-                  fontWeight: FontWeight.w800,
-                ),
-              ),
-              trailing: Container(
-                decoration: BoxDecoration(
-                  color: Colors.green.shade100,
-                  shape: BoxShape.circle,
-                ),
-                child: IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.notifications_none_rounded,
-                    size: 35,
-                    color: Color(greenColor),
-                  ),
-                ),
-              ),
-            ),
-            
-          ],
-        ),
-      ),
+      body: SafeArea(child: Column(children: [CustomListTileInfo()])),
     );
   }
 }
--- lib/features/shopping/presentation/views/widgets/custom_list_tile_info.dart
+++ lib/features/shopping/presentation/views/widgets/custom_list_tile_info.dart
@@ -1,1 +1,47 @@
+import 'package:flutter/material.dart';
 
+import '../../../../../core/constant/assets.dart';
+import '../../../../../core/constant/constant.dart';
+
+class CustomListTileInfo extends StatelessWidget {
+  const CustomListTileInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Image.asset(Assets.imagesAvatar),
+      title: Text(
+        'صباح الخير!..',
+        style: TextStyle(
+          fontFamily: 'Cairo',
+          fontSize: 20,
+          color: Colors.grey,
+          fontWeight: FontWeight.normal,
+        ),
+      ),
+      subtitle: Text(
+        'محمود ضاحي',
+        style: TextStyle(
+          fontFamily: 'Cairo',
+          fontSize: 24,
+          color: Colors.black,
+          fontWeight: FontWeight.w800,
+        ),
+      ),
+      trailing: Container(
+        decoration: BoxDecoration(
+          color: Colors.green.shade100,
+          shape: BoxShape.circle,
+        ),
+        child: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.notifications_none_rounded,
+            size: 35,
+            color: Color(greenColor),
+          ),
+        ),
+      ),
+    );
+  }
+}
