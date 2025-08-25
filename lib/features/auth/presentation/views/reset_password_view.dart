c0d3aae75856668efb97b5c3c3cc158d70f0c83b
Mahmoud Dahy <dahym2028@gmail.com>
2025-08-25 11:26:56 +0300

    refactor ResetPasswordView layout for improved readability and structure

--- lib/features/auth/presentation/views/reset_password_view.dart
+++ lib/features/auth/presentation/views/reset_password_view.dart
@@ -8,44 +8,47 @@ class ResetPasswordView extends StatelessWidget {
   static String id = "Reset Password";
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(backGroundColor),
-      appBar: AppBar(
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
         backgroundColor: Color(backGroundColor),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            icon: Icon(Icons.arrow_forward_ios),
-          ),
-        ],
-        automaticallyImplyLeading: false,
-        centerTitle: true,
-        title: Text(
-          'نسيان كلمة المرور',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            fontFamily: fontFamily,
+        appBar: AppBar(
+          backgroundColor: Color(backGroundColor),
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              icon: Icon(Icons.arrow_forward_ios),
+            ),
+          ],
+          automaticallyImplyLeading: false,
+          centerTitle: true,
+          title: Text(
+            'نسيان كلمة المرور',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              fontFamily: fontFamily,
+            ),
           ),
         ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(
-          children: [
-            const SizedBox(height: 20),
-            Text(
-              maxLines: 2,
-              'لا تقلق ما عليك سوي كتابة رقم هاتفك وسنرسل رمز التحقق.',
-              style: TextStyle(color: Color(0xff616A6B), fontSize: 24),
-            ),
-            const SizedBox(height: 20),
-            CustomTextField(hintText: 'ادخال رقم الهاتف', obscureText: false),
-            const SizedBox(height: 40),
-            CustomBotton(text: 'تأكيد', onTap: () {}),
-          ],
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              Text(
+                maxLines: 2,
+                'لا تقلق ما عليك سوي كتابة رقم هاتفك وسنرسل رمز التحقق.',
+                style: TextStyle(color: Color(0xff616A6B), fontSize: 24),
+              ),
+              const SizedBox(height: 20),
+              CustomTextField(hintText: 'ادخال رقم الهاتف', obscureText: false),
+              const SizedBox(height: 40),
+              CustomBotton(text: 'تأكيد', onTap: () {}),
+            ],
+          ),
         ),
       ),
     );
