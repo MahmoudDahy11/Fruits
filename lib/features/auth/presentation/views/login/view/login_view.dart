69784f9d7f9570233cbc1c577f16c073e2135b61
Mahmoud Dahy <dahym2028@gmail.com>
2025-08-25 11:26:59 +0300

    refactor LoginView to use BlocProvider for state management

--- lib/features/auth/presentation/views/login/view/login_view.dart
+++ lib/features/auth/presentation/views/login/view/login_view.dart
@@ -1,11 +1,19 @@
+import 'package:e_commerce_app/features/auth/data/repo/auth_repo_implement.dart';
+import 'package:e_commerce_app/features/auth/presentation/cubits/auth/auth_cubit.dart';
 import 'package:e_commerce_app/features/auth/presentation/views/login/view/login_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../../../core/services/firebase_auth.dart';
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
   static String id = 'Login_View';
   @override
   Widget build(BuildContext context) {
-    return LoginViewBody();
+    return BlocProvider(
+      create: (context) =>
+          AuthCubit(AuthRepoImplement(FirebaseAuthentication())),
+      child: LoginViewBody(),
+    );
   }
 }
