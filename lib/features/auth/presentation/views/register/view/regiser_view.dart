1ec40e23f29036716c91572ed18489f5733bf625
Mahmoud Dahy <dahym2028@gmail.com>
2025-08-25 11:27:03 +0300

    refactor RegiserView to use BlocProvider for state management

--- lib/features/auth/presentation/views/register/view/regiser_view.dart
+++ lib/features/auth/presentation/views/register/view/regiser_view.dart
@@ -1,11 +1,19 @@
+import 'package:e_commerce_app/core/services/firebase_auth.dart';
+import 'package:e_commerce_app/features/auth/data/repo/auth_repo_implement.dart';
+import 'package:e_commerce_app/features/auth/presentation/cubits/auth/auth_cubit.dart';
 import 'package:e_commerce_app/features/auth/presentation/views/register/view/register_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 
 class RegiserView extends StatelessWidget {
   const RegiserView({super.key});
   static String id = 'RegisterView';
   @override
   Widget build(BuildContext context) {
-    return RegisterViewBody();
+    return BlocProvider(
+      create: (context) => AuthCubit(AuthRepoImplement(FirebaseAuthentication()) ),
+      child: RegisterViewBody(),
+    );
   }
 }
