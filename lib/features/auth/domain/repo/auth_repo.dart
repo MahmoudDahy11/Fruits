1da48395496159bc69cfd3e40450b3a972942b85
Mahmoud Dahy <dahym2028@gmail.com>
2025-08-25 11:26:46 +0300

    add AuthRepo interface for user authentication methods

---
+++ lib/features/auth/domain/repo/auth_repo.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:e_commerce_app/core/errors/failure.dart';
+import 'package:e_commerce_app/features/auth/domain/entity/user_entity.dart';
+
+abstract class AuthRepo {
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword({
+    required String email,
+    required String password,
+    required String name,
+  });
+  Future<Either<Failure, UserEntity>> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+    required String fullName
+  });
+}
