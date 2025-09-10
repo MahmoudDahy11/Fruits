cb50facd898c380b5021f4ba1790683d30a98305
Mahmoud Dahy <dahym2028@gmail.com>
2025-09-10 12:58:32 +0300

    refactor: improve Firebase authentication methods and error handling

--- lib/core/services/firebase_auth.dart
+++ lib/core/services/firebase_auth.dart
@@ -1,29 +1,58 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:e_commerce_app/core/errors/custom_excption.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-class FirebaseAuthentication {
+class FirebaseService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
   Future<User> createUserWithEmailAndPassword({
     required String email,
     required String password,
+    required String name,
   }) async {
     try {
-      final credential = await FirebaseAuth.instance
+      final UserCredential credential = await _auth
           .createUserWithEmailAndPassword(email: email, password: password);
-      return credential.user!;
+
+      final user = credential.user;
+      if (user == null) {
+        throw CustomException(errMessage: 'User creation failed.');
+      }
+
+      await user.updateDisplayName(name);
+      await user.reload();
+      log("✅ User created: ${user.email}");
+      await user.sendEmailVerification();
+      log("📩 Verification email sent to: ${user.email}");
+      return user;
     } on FirebaseAuthException catch (e) {
-      if (e.code == 'weak-password') {
-        throw (CustomExcption(
-          errMessage: 'The password provided is too weak.',
-        ));
-      } else if (e.code == 'email-already-in-use') {
-        throw (CustomExcption(
-          errMessage: 'The account already exists for that email.',
-        ));
-      } else {
-        throw (CustomExcption(errMessage: 'FirebaseAuth error'));
+      switch (e.code) {
+        case 'weak-password':
+          throw CustomException(
+            errMessage: 'The password provided is too weak.',
+          );
+        case 'email-already-in-use':
+          throw CustomException(
+            errMessage: 'The account already exists for that email.',
+          );
+        case 'invalid-email':
+          throw CustomException(errMessage: 'The email address is invalid.');
+        case 'operation-not-allowed':
+          throw CustomException(
+            errMessage: 'Email/Password accounts are not enabled.',
+          );
+        default:
+          throw CustomException(
+            errMessage: e.message ?? 'Unknown FirebaseAuth error.',
+          );
       }
     } catch (e) {
-      throw (CustomExcption(errMessage: 'Oops there was an erroe !!'));
+      throw CustomException(errMessage: e.toString());
     }
   }
 
@@ -32,23 +61,130 @@ class FirebaseAuthentication {
     required String password,
   }) async {
     try {
-      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+      final UserCredential credential = await _auth.signInWithEmailAndPassword(
         email: email,
         password: password,
       );
-      return credential.user!;
+
+      final user = credential.user;
+      if (user == null) {
+        throw CustomException(errMessage: 'Sign in failed. Please try again.');
+      }
+
+      await user.reload();
+
+      if (!user.emailVerified) {
+        throw CustomException(
+          errMessage: 'Please verify your email before logging in.',
+        );
+      }
+      await firestore.collection('users').doc(user.uid).set({
+        'uid': user.uid,
+        'email': user.email,
+        'name': user.displayName ?? '',
+        'createdAt': FieldValue.serverTimestamp(),
+      }, SetOptions(merge: true));
+
+      return user;
     } on FirebaseAuthException catch (e) {
-      if (e.code == 'user-not-found') {
-        throw (CustomExcption(errMessage: 'No user found for that email.'));
-      } else if (e.code == 'wrong-password') {
-        throw (CustomExcption(
-          errMessage: 'Wrong password provided for that user.',
-        ));
-      } else {
-        throw (CustomExcption(errMessage: 'FirebaseAuth error'));
+      switch (e.code) {
+        case 'invalid-email':
+          throw CustomException(errMessage: 'The email address is invalid.');
+        case 'user-disabled':
+          throw CustomException(
+            errMessage: 'This user account has been disabled.',
+          );
+        case 'user-not-found':
+          throw CustomException(errMessage: 'No user found for that email.');
+        case 'wrong-password':
+          throw CustomException(errMessage: 'Wrong password provided.');
+        default:
+          throw CustomException(
+            errMessage: e.message ?? 'Unknown FirebaseAuth error.',
+          );
       }
     } catch (e) {
-      throw (CustomExcption(errMessage: 'Oops there was an erroe !!'));
+      throw CustomException(errMessage: e.toString());
+    }
+  }
+
+  Future<void> signOut() async {
+    try {
+      await _auth.signOut();
+      await GoogleSignIn().signOut();
+      await FacebookAuth.instance.logOut();
+    } catch (e) {
+      throw CustomException(errMessage: 'Sign out failed: ${e.toString()}');
+    }
+  }
+
+  Future<User> signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      if (googleUser == null) {
+        throw CustomException(errMessage: "Google sign-in was cancelled.");
+      }
+
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      final UserCredential userCredential = await _auth.signInWithCredential(
+        credential,
+      );
+      final user = userCredential.user;
+
+      if (user == null) {
+        throw CustomException(
+          errMessage: "Google sign-in failed. No user returned.",
+        );
+      }
+
+      await firestore.collection('users').doc(user.uid).set({
+        'uid': user.uid,
+        'email': user.email,
+        'name': user.displayName ?? '',
+        'photoUrl': user.photoURL ?? '',
+        'createdAt': FieldValue.serverTimestamp(),
+      }, SetOptions(merge: true));
+
+      return user;
+    } on FirebaseAuthException catch (e) {
+      throw CustomException(
+        errMessage: e.message ?? "FirebaseAuth Google error.",
+      );
+    } catch (e) {
+      throw CustomException(errMessage: "Google sign-in failed: $e");
+    }
+  }
+
+  Future<UserCredential> signInWithFacebook() async {
+    try {
+      final LoginResult result = await FacebookAuth.instance.login();
+      log("Facebook login status: ${result.status}");
+      log("Facebook login message: ${result.message}");
+      if (result.status == LoginStatus.success) {
+        final AccessToken accessToken = result.accessToken!;
+
+        final OAuthCredential credential = FacebookAuthProvider.credential(
+          accessToken.tokenString,
+        );
+
+        return await FirebaseAuth.instance.signInWithCredential(credential);
+      } else if (result.status == LoginStatus.cancelled) {
+        throw CustomException(errMessage: 'Facebook sign-in was cancelled.');
+      } else {
+        throw CustomException(
+          errMessage: result.message ?? 'Unknown Facebook login error.',
+        );
+      }
+    } catch (e) {
+      throw CustomException(errMessage: e.toString());
     }
   }
 }
