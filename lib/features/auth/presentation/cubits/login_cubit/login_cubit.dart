9b1342d7ac34f00d631b8693c7f0063ccab80dc5
Mahmoud Dahy <dahym2028@gmail.com>
2025-09-10 14:07:58 +0300

    create sign in cubit and handle all states

---
+++ lib/features/auth/presentation/cubits/login_cubit/login_cubit.dart
@@ -0,0 +1,29 @@
+import 'package:bloc/bloc.dart';
+import 'package:e_commerce_app/features/auth/domain/entity/user_entity.dart';
+import 'package:e_commerce_app/features/auth/domain/repo/auth_repo.dart';
+import 'package:meta/meta.dart';
+
+
+
+part 'login_state.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  LoginCubit(this._firebaseAuthrepo) : super(LoginInitial());
+
+  final FirebaseAuthRepo _firebaseAuthrepo;
+
+  Future<void> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    emit(LoginLoading());
+    final result = await _firebaseAuthrepo.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    result.fold(
+      (failure) => emit(LoginFailure(errMessage: failure.errMessage)),
+      (user) => emit(LoginSuccess(user)),
+    );
+  }
+}
