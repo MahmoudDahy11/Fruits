4eac923775973d9487d9bc24a1e1fbed209be3bb
Mahmoud Dahy <dahym2028@gmail.com>
2025-09-10 12:59:35 +0300

    implement all method

--- lib/features/auth/domain/repo/auth_repo.dart
+++ lib/features/auth/domain/repo/auth_repo.dart
@@ -2,15 +2,17 @@ import 'package:dartz/dartz.dart';
 import 'package:e_commerce_app/core/errors/failure.dart';
 import 'package:e_commerce_app/features/auth/domain/entity/user_entity.dart';
 
-abstract class AuthRepo {
-  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword({
+abstract class FirebaseAuthRepo {
+  Future<Either<CustomFailure, UserEntity>> createUserWithEmailAndPassword({
     required String email,
     required String password,
     required String name,
   });
-  Future<Either<Failure, UserEntity>> signInWithEmailAndPassword({
+  Future<Either<CustomFailure, UserEntity>> signInWithEmailAndPassword({
     required String email,
     required String password,
-    required String fullName
   });
+  Future<Either<CustomFailure, Unit>> signOut();
+  Future<Either<CustomFailure, Unit>> signInWithGoogle();
+  Future<Either<CustomFailure, UserEntity>> signinWithFacebook();
 }
