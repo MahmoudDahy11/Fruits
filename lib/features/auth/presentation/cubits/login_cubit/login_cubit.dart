2879eb548285174b8e1243618e6373e981ba7a6e
Mahmoud Dahy <dahym2028@gmail.com>
2025-09-10 21:11:48 +0300

    refactor

--- lib/features/auth/presentation/cubits/login_cubit/login_cubit.dart
+++ lib/features/auth/presentation/cubits/login_cubit/login_cubit.dart
@@ -3,8 +3,6 @@ import 'package:e_commerce_app/features/auth/domain/entity/user_entity.dart';
 import 'package:e_commerce_app/features/auth/domain/repo/auth_repo.dart';
 import 'package:meta/meta.dart';
 
-
-
 part 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
@@ -16,14 +14,18 @@ class LoginCubit extends Cubit<LoginState> {
     required String email,
     required String password,
   }) async {
-    emit(LoginLoading());
-    final result = await _firebaseAuthrepo.signInWithEmailAndPassword(
-      email: email,
-      password: password,
-    );
-    result.fold(
-      (failure) => emit(LoginFailure(errMessage: failure.errMessage)),
-      (user) => emit(LoginSuccess(user)),
-    );
+    try {
+      emit(LoginLoading());
+      final result = await _firebaseAuthrepo.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      result.fold(
+        (failure) => emit(LoginFailure(errMessage: failure.errMessage)),
+        (user) => emit(LoginSuccess(user)),
+      );
+    } catch (e) {
+      emit(LoginFailure(errMessage: e.toString()));
+    }
   }
 }
