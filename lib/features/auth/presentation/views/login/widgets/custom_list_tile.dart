56849ec5c1cedb66f7b0b8e38dccee44faefce2a
Mahmoud Dahy <dahym2028@gmail.com>
2025-09-10 21:17:04 +0300

    add onTap

--- lib/features/auth/presentation/views/login/widgets/custom_list_tile.dart
+++ lib/features/auth/presentation/views/login/widgets/custom_list_tile.dart
@@ -4,32 +4,34 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 class CustomListTile extends StatelessWidget {
-  const CustomListTile({super.key});
-
+  const CustomListTile({super.key, this.onTap});
+  final VoidCallback? onTap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey.shade400),
-        color: Color(backGroundColor),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: ListTile(
-        leading: SvgPicture.asset(
-          Assets.imagesGoogleIcons,
-          width: 30,
-          height: 30,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey.shade400),
+          color: Color(backGroundColor),
+          borderRadius: BorderRadius.circular(16),
         ),
-        title: const Text(
-          'تسجيل الدخول باستخدام جوجل',
-          style: TextStyle(
-            fontSize: 20,
-            fontFamily: fontFamily,
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
+        child: ListTile(
+          leading: SvgPicture.asset(
+            Assets.imagesGoogleIcons,
+            width: 30,
+            height: 30,
+          ),
+          title: const Text(
+            'تسجيل الدخول باستخدام جوجل',
+            style: TextStyle(
+              fontSize: 20,
+              fontFamily: fontFamily,
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
-        onTap: () {},
       ),
     );
   }
