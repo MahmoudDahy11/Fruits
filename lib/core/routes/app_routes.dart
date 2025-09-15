06da938c5897bc3fbdc5ea04af9aa466b1440b0c
Mahmoud Dahy <dahym2028@gmail.com>
2025-09-15 17:56:16 +0300

    pass product entity

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -2,6 +2,9 @@ import 'package:e_commerce_app/core/services/get_it.dart';
 import 'package:e_commerce_app/features/auth/presentation/views/login/view/login_view.dart';
 import 'package:e_commerce_app/features/auth/presentation/views/reset_password_view.dart';
 import 'package:e_commerce_app/features/on_boarding/presentation/page_view.dart';
+import 'package:e_commerce_app/features/shopping/domain/entity/product_entity.dart';
+import 'package:e_commerce_app/features/shopping/presentation/cubits/add_product/add_product_cubit.dart';
+import 'package:e_commerce_app/features/shopping/presentation/views/cart_view.dart';
 import 'package:e_commerce_app/features/shopping/presentation/views/home.dart';
 import 'package:e_commerce_app/features/shopping/product_view.dart';
 import 'package:e_commerce_app/features/splash/presentation/views/splash.dart';
@@ -13,9 +16,8 @@ import '../../features/auth/presentation/cubits/login_cubit/login_cubit.dart';
 import '../../features/auth/presentation/cubits/signout_cubit/signout_cubit.dart';
 import '../../features/auth/presentation/cubits/signup_cubit/signup_cubit.dart';
 import '../../features/auth/presentation/views/register/view/regiser_view.dart';
+import '../../features/shopping/presentation/cubits/get_product/get_product_cubit.dart';
 import '../../features/shopping/presentation/views/product_details_view.dart';
-
-
 
 /*
  * AppRoutes class
@@ -36,9 +38,27 @@ class AppRoutes {
       child: const LoginView(),
     ),
     ResetPasswordView.id: (context) => const ResetPasswordView(),
-    ProductDetiallsView.id: (context) => const ProductDetiallsView(),
-    HomeView.id: (context) => const HomeView(),
-    ProductsView.id: (context) => const ProductsView(),
+    ProductDetiallsView.id: (context) {
+      final product =
+          ModalRoute.of(context)!.settings.arguments as ProductEntity;
+      return ProductDetiallsView(product: product);
+    },
+
+    HomeViewProduct.id: (context) => MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => getIt<GetProductCubit>()),
+        BlocProvider(create: (context) => getIt<AddProductCubit>()),
+      ],
+      child: const HomeViewProduct(),
+    ),
+    ProductsView.id: (context) => MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => getIt<GetProductCubit>()),
+        BlocProvider(create: (context) => getIt<AddProductCubit>()),
+      ],
+      child: const ProductsView(),
+    ),
+    CartView.id: (context) => const CartView(),
     RegiserView.id: (context) => MultiBlocProvider(
       providers: [
         BlocProvider(create: (context) => getIt<SignupCubit>()),
