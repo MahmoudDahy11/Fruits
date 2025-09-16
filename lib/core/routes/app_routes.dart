9cc0551f5cc50cf2086c20a4a70e22f92d5ccf51
Mahmoud Dahy <dahym2028@gmail.com>
2025-09-16 16:14:17 +0300

    provide CartCubit

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -4,12 +4,14 @@ import 'package:e_commerce_app/features/auth/presentation/views/reset_password_v
 import 'package:e_commerce_app/features/on_boarding/presentation/page_view.dart';
 import 'package:e_commerce_app/features/shopping/domain/entity/product_entity.dart';
 import 'package:e_commerce_app/features/shopping/presentation/cubits/add_product/add_product_cubit.dart';
+import 'package:e_commerce_app/features/shopping/presentation/cubits/cart/cart_cubit.dart';
 import 'package:e_commerce_app/features/shopping/presentation/views/cart_view.dart';
-import 'package:e_commerce_app/features/shopping/presentation/views/home.dart';
-import 'package:e_commerce_app/features/shopping/product_view.dart';
+import 'package:e_commerce_app/features/shopping/presentation/views/home_view_product.dart';
+import 'package:e_commerce_app/features/shopping/presentation/views/product_view.dart';
 import 'package:e_commerce_app/features/splash/presentation/views/splash.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 import '../../features/auth/presentation/cubits/facebook_cubit/facebook_cubit.dart';
 import '../../features/auth/presentation/cubits/google_cubit/google_cubit.dart';
 import '../../features/auth/presentation/cubits/login_cubit/login_cubit.dart';
@@ -28,6 +30,7 @@ class AppRoutes {
   static Map<String, WidgetBuilder> routes = {
     Splash.id: (context) => const Splash(),
     OnBoarding.id: (context) => const OnBoarding(),
+
     LoginView.id: (context) => MultiBlocProvider(
       providers: [
         BlocProvider(create: (context) => getIt<LoginCubit>()),
@@ -37,28 +40,45 @@ class AppRoutes {
       ],
       child: const LoginView(),
     ),
+
     ResetPasswordView.id: (context) => const ResetPasswordView(),
+
     ProductDetiallsView.id: (context) {
       final product =
           ModalRoute.of(context)!.settings.arguments as ProductEntity;
-      return ProductDetiallsView(product: product);
+
+      return MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (context) => getIt<AddProductCubit>()),
+          BlocProvider(create: (context) => getIt<CartCubit>()),
+        ],
+        child: ProductDetiallsView(product: product),
+      );
     },
 
     HomeViewProduct.id: (context) => MultiBlocProvider(
       providers: [
         BlocProvider(create: (context) => getIt<GetProductCubit>()),
         BlocProvider(create: (context) => getIt<AddProductCubit>()),
+        BlocProvider(create: (context) => getIt<CartCubit>()),
       ],
       child: const HomeViewProduct(),
     ),
+
     ProductsView.id: (context) => MultiBlocProvider(
       providers: [
         BlocProvider(create: (context) => getIt<GetProductCubit>()),
         BlocProvider(create: (context) => getIt<AddProductCubit>()),
+        BlocProvider(create: (context) => getIt<CartCubit>()),
       ],
       child: const ProductsView(),
     ),
-    CartView.id: (context) => const CartView(),
+
+    CartView.id: (context) => BlocProvider(
+      create: (context) => getIt<CartCubit>(),
+      child: const CartView(),
+    ),
+
     RegiserView.id: (context) => MultiBlocProvider(
       providers: [
         BlocProvider(create: (context) => getIt<SignupCubit>()),
