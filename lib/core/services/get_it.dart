2e6a4f02bd98688d91ba853ac77a0cf1dc20e55c
Mahmoud Dahy <dahym2028@gmail.com>
2025-09-16 16:14:56 +0300

    add CartFirestoreService ,CartRepositoryImpl , CartCubit

--- lib/core/services/get_it.dart
+++ lib/core/services/get_it.dart
@@ -3,6 +3,7 @@ import 'package:e_commerce_app/core/services/api_service.dart';
 import 'package:e_commerce_app/core/services/firebase_auth.dart';
 import 'package:e_commerce_app/features/auth/data/repo/auth_repo_implement.dart';
 import 'package:e_commerce_app/features/auth/domain/repo/auth_repo.dart';
+import 'package:e_commerce_app/features/shopping/data/repo/cart_repository_implement.dart';
 import 'package:e_commerce_app/features/shopping/data/repo/product_repo_implement.dart';
 import 'package:e_commerce_app/features/shopping/domain/repo/product_repo.dart';
 import 'package:e_commerce_app/features/shopping/presentation/cubits/add_product/add_product_cubit.dart';
@@ -17,6 +18,9 @@ import '../../features/auth/presentation/cubits/google_cubit/google_cubit.dart';
 import '../../features/auth/presentation/cubits/login_cubit/login_cubit.dart';
 import '../../features/auth/presentation/cubits/signout_cubit/signout_cubit.dart';
 import '../../features/auth/presentation/cubits/signup_cubit/signup_cubit.dart';
+import '../../features/shopping/data/service/cart_service.dart';
+import '../../features/shopping/domain/repo/cart_repository.dart';
+import '../../features/shopping/presentation/cubits/cart/cart_cubit.dart';
 
 final getIt = GetIt.instance;
 
@@ -29,12 +33,18 @@ Future<void> getItSetup() async {
   // services
   getIt.registerLazySingleton<FirebaseService>(() => FirebaseService());
   getIt.registerLazySingleton<ApiService>(() => ApiService(Dio()));
+  getIt.registerLazySingleton<CartFirestoreService>(
+    () => CartFirestoreService(),
+  );
 
   // repo
   getIt.registerLazySingleton<FirebaseAuthRepo>(
     () => FirebaseAuthRepoImplement(getIt()),
   );
   getIt.registerLazySingleton<ProductRepo>(() => ProductRepoImplement(getIt()));
+  getIt.registerLazySingleton<CartRepository>(
+    () => CartRepositoryImpl(service: getIt<CartFirestoreService>()),
+  );
 
   /// Cubits
   getIt.registerFactory(() => SignupCubit(getIt()));
@@ -48,4 +58,5 @@ Future<void> getItSetup() async {
   getIt.registerFactory(() => GetProductCubit(getIt()));
   getIt.registerFactory(() => PutProductCubit(getIt()));
   getIt.registerFactory(() => PatchProductCubit(getIt()));
+  getIt.registerFactory(() => CartCubit(getIt()));
 }
